bced0ece3206a28b48fccda7b9514bde5714cf27
Brian Mirabito <bjm9265@g.rit.edu>
2020-12-22 15:42:22 -0500

    cleaned up project boilerplate, added navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+// components
+import Nav from "./components/Nav/Nav";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Nav />
+      My Portfolio
     </div>
   );
-}
+};
 
 export default App;
